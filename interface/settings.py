a706f93b26241ef2d2a13b4d5a5d69b6e661aa95
Aniczk <32680745+Aniczk@users.noreply.github.com>
2020-09-14 16:36:40 +0200

    Add files via upload

--- interface/settings.py
+++ interface/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR
+]
+
+STATIC_ROOT = "var/www/bioputer.mimuw.edu.pl:8000/static/"
--- interface/urls.py
+++ interface/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from uniprotsearch import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^$', views.search),
-]
+    url('about/', views.about, name='about')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
