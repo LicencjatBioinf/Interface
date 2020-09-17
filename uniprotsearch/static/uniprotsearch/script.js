5fff5cf143c55f90338f93b0e5d33ccfdeb21c78
Aniczk <32680745+Aniczk@users.noreply.github.com>
2020-09-17 13:03:01 +0200

    Add files via upload

--- uniprotsearch/static/uniprotsearch/script.js
+++ uniprotsearch/static/uniprotsearch/script.js
@@ -11,3 +11,19 @@ function myFunction() {
         f.style.display = "block";
     }                
 }
+
+// Informacje w dymkach, które wyświetlają się po najechaniu na ikonkę informacyjną.
+(function ($) {
+    $(document).ready(function(){
+    $("#spectrum_input_file-span").text("Upload a file with non-fragmented spectrum in mzXML format.");
+    $("#max_wasserstain_distance-span").text("Specify the maximum Wasserstain distance.");
+    $("#theoretical_spectrum_coverage-span").text("A fraction specifying what part of the theoretical spectrum will be generated. The unit: %.");
+    $("#organism-span").text("Only this group will be searched.");
+    $("#search_area-span").text("Specify the search source.");
+    $("#database-span").text("Select the name of the public database.");
+    $("#fasta_input_file-span").text("Upload your own file in fasta format.");
+    $("#max_results-span").text("Specify the maximum number of results that will appear on the page.");
+    $("#enzymatic_activity-span").text("Determining if a protein has enzymatic activity. Attention! It doesn't work yet! We will add this functionality soon.");
+    $("#posttranslational_modifications-span").text("Choose post-translational modifications. Attention! It doesn't work yet! We will add this functionality soon.");
+    });
+}(jQuery))
